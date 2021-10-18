d4da90a3c726a40c0f5bc871bef0d179dcc67336
ahmedgamaml9055 <ahmed.gamal9@hotmail.com>
2021-10-18 11:00:43 +0200

    first update
    ..

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -239,7 +239,7 @@ class _HomeState extends State<Home> {
 
               return new Center(
                 child: new Text(
-                  "No Notes has been found.",
+                  "No Notes has been found",
                 ),
               );
 
